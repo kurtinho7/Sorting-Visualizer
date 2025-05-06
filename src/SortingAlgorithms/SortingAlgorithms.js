de56b8e652084a7ebc11efe6744727ad55369a6d
Kurt Hymanyk <khymanyk@student.ubc.ca>
2025-05-06 17:39:45 -0600

    Finished Selection Sort Animation

--- src/SortingAlgorithms/SortingAlgorithms.js
+++ src/SortingAlgorithms/SortingAlgorithms.js
@@ -1,41 +1,128 @@
-function merge(left, right) {
-    const mergedArray = [];
-    let i = 0;
-    let j = 0;
+// function merge(left, right) {
+//     const mergedArray = [];
+//     let i = 0;
+//     let j = 0;
     
-    while (i < left.length && j < right.length) {
-        if (left[i] <= right[j]) {
-            mergedArray.push(left[i]);
-            i++
+//     while (i < left.length && j < right.length) {
+//         if (left[i] <= right[j]) {
+//             mergedArray.push(left[i]);
+//             i++
+//         } else {
+//             mergedArray.push(right[j]);
+//             j++
+//         }
+//     }
+
+//     if (i < left.length) {
+//         for (let x = i; x < left.length; x++) {
+//             mergedArray.push(left[x]);
+//         }
+//     }
+//     if (j < right.length) {
+//         for (let y = j; y < right.length; y++) {
+//             mergedArray.push(right[y]);
+//         }
+//     }
+
+//     return mergedArray;
+// }
+
+// export const mergeSort = array => {
+//     if (array.length <= 1) {
+//         return array;
+//     }
+    
+//     const middleIdx = Math.floor(array.length/2);
+//     const firstHalf = mergeSort(array.slice(0, middleIdx));
+//     const secondHalf = mergeSort(array.slice(middleIdx));
+
+//     return merge(firstHalf, secondHalf);
+
+// };
+
+
+function merge(mainArray, startIdx, middleIdx, endIdx, overallArray, animations) {
+    let k = startIdx
+    let i = startIdx;
+    let j = middleIdx + 1;
+    
+    while (i <= middleIdx && j <= endIdx) {
+        animations.push([i, j]);
+        animations.push([i, j]);
+        if (overallArray[i] <= overallArray[j]) {
+            animations.push([k, overallArray[i]]);
+            mainArray[k++] = overallArray[i++];
         } else {
-            mergedArray.push(right[j]);
-            j++
+            animations.push([k, overallArray[j]]);
+            mainArray[k++] = overallArray[j++];
         }
     }
 
-    if (i < left.length) {
-        for (let x = i; x < left.length; x++) {
-            mergedArray.push(left[x]);
-        }
-    }
-    if (j < right.length) {
-        for (let y = j; y < right.length; y++) {
-            mergedArray.push(right[y]);
-        }
-    }
+    while (i <= middleIdx) {
+        animations.push([i, i]);
+        animations.push([i, i]);
+        animations.push([k, overallArray[i]]);
 
-    return mergedArray;
+        mainArray[k++] = overallArray[i++];
+    }
+    while (j <= endIdx) {
+        animations.push([j, j]);
+        animations.push([j, j]);
+        animations.push([k, overallArray[j]]);
+        mainArray[k++] = overallArray[j++];
+    }
 }
 
-export const mergeSort = array => {
-    if (array.length <= 1) {
-        return array;
+function mergeSortHelper(mainArray, startIdx, endIdx, overallArray, animations) {
+    if (startIdx === endIdx) {
+        return;
     }
-    
-    const middleIdx = Math.floor(array.length/2);
-    const firstHalf = mergeSort(array.slice(0, middleIdx));
-    const secondHalf = mergeSort(array.slice(middleIdx));
+    const middleIdx = Math.floor((startIdx + endIdx) / 2);
+    mergeSortHelper(overallArray, startIdx, middleIdx, mainArray, animations);
+    mergeSortHelper(overallArray, middleIdx + 1, endIdx, mainArray, animations);
+    merge(mainArray, startIdx, middleIdx, endIdx, overallArray, animations);
 
-    return merge(firstHalf, secondHalf);
+}
 
-};
+
+
+export function mergeSort(array) {
+    const animations = [];
+    if (array.length <= 1) {
+        return animations;
+    }
+    const overallArray = array.slice();
+    mergeSortHelper(array, 0, array.length - 1, overallArray, animations);
+    return animations;
+}
+
+
+export function selectionSort(array) {
+    const animations = [];
+    if (array.length <= 1) {
+        return animations;
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+        let minIdx = i;
+        for (let j = i + 1; j < array.length; j++) {
+            const animation1 = {};
+            animation1.comparison = [minIdx, j];
+            if (array[j] < array[minIdx]) {
+                minIdx = j;
+                const animation2 = {};
+                animation2.minChange = [minIdx, j];
+                animations.push(animation2);
+            }
+            animations.push(animation1);
+        }
+        const animation = {};
+        animation.swap = [i, minIdx];
+        let temp = array[i];
+        array[i] = array[minIdx];
+        array[minIdx] = temp;
+        animations.push(animation);
+    }
+
+    return animations;
+
+}
--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './SortingVisualizer.css';
-import { mergeSort } from "../SortingAlgorithms/SortingAlgorithms";
+import { mergeSort, selectionSort } from "../SortingAlgorithms/SortingAlgorithms";
 
 export class SortingVisualizer extends React.Component {
     constructor(props) {
@@ -18,7 +18,7 @@ export class SortingVisualizer extends React.Component {
     resetArray() {
         const array = [];
         for (let i = 0; i < 250; i++) {
-            array.push(randomIntFromInterval(5, 100));
+            array.push(randomIntFromInterval(5, 700));
         }
         this.setState({array});
     }
@@ -27,13 +27,30 @@ export class SortingVisualizer extends React.Component {
 
     }
 
+    // Inspired by https://github.com/clementmihailescu/Sorting-Visualizer-Tutorial
     mergeSort() {
+        const animations = mergeSort(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName("arrayBar");
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneId, barTwoId] = animations[i];
+                const barOneStyle = arrayBars[barOneId].style;
+                const barTwoStyle = arrayBars[barTwoId].style;
+                const color = i % 3 === 0 ? 'red' : 'turquoise';
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * 3);
+            } else {
+                setTimeout(() => {
+                    const [barOneId, barTwoHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneId].style;
+                    barOneStyle.height = `${barTwoHeight}px`;
 
-        const sortedArray = mergeSort(this.state.array);
-        const javaScriptSort = this.state.array.slice().sort(function(a, b){return a - b});
-
-        console.log(arraysAreEqual(sortedArray, javaScriptSort));
-
+                }, i * 3);
+            }
+        }
     }
 
     bubbleSort() {
@@ -41,6 +58,52 @@ export class SortingVisualizer extends React.Component {
     }
 
     selectionSort() {
+        const animations = selectionSort(this.state.array.slice());
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName("arrayBar");
+            const animation = animations[i];
+            const hasComparison = animation.hasOwnProperty("comparison");
+            const hasChangeMin = animation.hasOwnProperty("changeMin");
+            const hasSwap = animation.hasOwnProperty("swap");
+
+            if (hasComparison) {
+                const [barOneId, barTwoId] = animation.comparison;
+                const barOneStyle = arrayBars[barOneId].style;
+                const barTwoStyle = arrayBars[barTwoId].style;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = 'red';
+                    barTwoStyle.backgroundColor = 'red';
+                }, i * 1);
+
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = 'turquoise';
+                    barTwoStyle.backgroundColor = 'turquoise';
+                }, (i + 1) * 1);
+
+
+            } else if (hasChangeMin) {
+                const [barOneId, barTwoId] = animation.minChange;
+                const barTwoStyle = arrayBars[barTwoId].style;
+                setTimeout(() => {
+                    barTwoStyle.backgroundColor = 'gold';
+                }, i * 5);
+
+                setTimeout(() => {
+                    barTwoStyle.backgroundColor = 'turquoise';
+                }, (i + 1) * 5);
+
+            } else if (hasSwap) {
+                setTimeout(() => {
+                    const [barOneId, barTwoId] = animation.swap;
+                    const barOneStyle = arrayBars[barOneId].style;
+                    const barTwoStyle = arrayBars[barTwoId].style;
+                    const tempHeight = barOneStyle.height;
+                    barOneStyle.height = barTwoStyle.height;
+                    barTwoStyle.height = tempHeight;
+
+                }, i * 1);
+            }
+        }
 
     }
 
@@ -63,7 +126,7 @@ export class SortingVisualizer extends React.Component {
                   <div
                     className="arrayBar"
                     key={idx}
-                    style={{ height: `${value}%` }}
+                    style={{ height: `${value}px` }}
                   />
                 ))}
               </div>
